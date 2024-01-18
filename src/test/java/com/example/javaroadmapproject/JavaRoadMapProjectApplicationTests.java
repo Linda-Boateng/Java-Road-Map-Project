b9a644cc28dd715027d15b78910aa21f6634da71
Linda-Boateng <linda.boateng@amalitech.com>
2024-01-18 21:28:35 +0000

    Project Setup

---
+++ src/test/java/com/example/javaroadmapproject/JavaRoadMapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaroadmapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRoadMapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
