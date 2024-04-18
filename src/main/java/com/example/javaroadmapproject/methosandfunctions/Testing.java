cee53e4cd720dbc491f584e186d217adc8ee357f
Linda-Boateng <linda.boateng@amalitech.com>
2024-04-18 09:41:17 +0100

    added method implementation in java

---
+++ src/main/java/com/example/javaroadmapproject/methosandfunctions/Testing.java
@@ -0,0 +1,30 @@
+package com.example.javaroadmapproject.methosandfunctions;
+
+public class Testing {
+    public static void main(String[] args){
+        SavingsAccount savings = new SavingsAccount(2000);
+
+        //Check balance:
+        savings.checkBalance();
+
+        //Withdrawing:
+        savings.withdraw(300);
+
+        //Check balance:
+        savings.checkBalance();
+
+        //Deposit:
+        savings.deposit(600);
+
+        //Check balance:
+        savings.checkBalance();
+
+        //Deposit:
+        savings.deposit(600);
+
+        //Check balance:
+        savings.checkBalance();
+
+        System.out.println(savings);
+    }
+}
